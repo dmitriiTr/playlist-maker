7a49bf3a4a2404c73ddfe76d7352547e73627d69
dmitriiTr <56300102+dmitriiTr@users.noreply.github.com>
2024-05-02 09:40:44 +0300

    Selecting audio and disabling subtitles

--- index.js
+++ index.js
@@ -3,19 +3,25 @@ import { existsSync, readdirSync, writeFileSync } from 'node:fs';
 import { create } from 'xmlbuilder2';
 
 const rootFolder = process.argv[2];
+const audioTrack = process.argv[3] ?? 0;
+const subsFlag = process.argv[4] === "true";
 const folderNamesReaden = readdirSync(rootFolder);
 
 /**
- * @param {string} folder - name of folder
+ * @param {string} path - name of folder
  * @return {string[]} list of all videofiles in folder
  */
-function getVideosNames(folder) {
+function getVideosNames(path) {
   try {
-    if (!existsSync(folder)) {
+    if (!existsSync(path)) {
       console.log('not found');
     }
     else {
-      return readdirSync(folder).filter(name => name.indexOf('.mp4') !== -1);
+      // todo: { withFileTypes: true } and recoursion
+      return readdirSync(path) 
+      .filter(name => name.indexOf('.mp4') !== -1 || name.indexOf('.mkv') !== -1)
+      // Without addning global path subs do not work for some reason
+      .map(name => `${path}/${name}`);
     }
   } catch (err) {
     console.error(err);
@@ -28,6 +34,7 @@ function getVideosNames(folder) {
  * @return {string} playlist in xml format 
  */
 function createPlaylistXML(fileNames) {
+  const fakeSubTrack = 99;
   const root = create({ version: '1.0', encoding: "UTF-8" })
     .ele('playlist', { xmlns: 'http://xspf.org/ns/0/', "xmlns:vlc": "http://www.videolan.org/vlc/playlist/ns/0/" })
     .ele('title').txt("Title").up().ele('trackList');
@@ -36,8 +43,13 @@ function createPlaylistXML(fileNames) {
     root.ele('track')
       .ele('location').txt(`file:///${file}`).up()
       .ele('duration').txt("0").up()
-      .ele('extension', { "application": "http://www.videolan.org/vlc/playlist/0" }).ele("vlc:id").txt(i).up().up()
-      .up();
+      .ele('extension', { "application": "http://www.videolan.org/vlc/playlist/0" })
+        .ele("vlc:id").txt(i).up()
+        .ele("vlc:option").txt(`audio-track=${audioTrack}`).up()
+        // Setting sub track to fake number to turn off subtitles by default
+        .ele("vlc:option").txt(`sub-track=${subsFlag ? 0 : fakeSubTrack}`).up()
+      .up()
+    .up();
   })
 
   root.up()
