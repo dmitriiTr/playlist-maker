8fa1c75322555442bfac4534ba266fd7abc58800
dmitriiTr <56300102+dmitriiTr@users.noreply.github.com>
2024-05-02 12:46:24 +0300

    Can now create playlists in subfolders by recursion

--- index.js
+++ index.js
@@ -5,23 +5,30 @@ import { create } from 'xmlbuilder2';
 const rootFolder = process.argv[2];
 const audioTrack = process.argv[3] ?? 0;
 const subsFlag = process.argv[4] === "true";
-const folderNamesReaden = readdirSync(rootFolder);
+const recFlag = process.argv[5] === "true";
 
 /**
- * @param {string} path - name of folder
+ * @param {string} pathToVideos - path to folder with videos
  * @return {string[]} list of all videofiles in folder
  */
-function getVideosNames(path) {
+function getVideosNames(pathToVideos) {
   try {
-    if (!existsSync(path)) {
+    if (!existsSync(pathToVideos)) {
       console.log('not found');
     }
     else {
-      // todo: { withFileTypes: true } and recoursion
-      return readdirSync(path) 
-      .filter(name => name.indexOf('.mp4') !== -1 || name.indexOf('.mkv') !== -1)
-      // Without addning global path subs do not work for some reason
-      .map(name => `${path}/${name}`);
+      const all = readdirSync(pathToVideos, { withFileTypes: true });
+      const dirs = all.filter(n => n.isDirectory());
+      if (recFlag && dirs.length !== 0) {
+        // recursively creating playlists for subfolders
+        dirs.forEach(dir => createPlaylistFiles(`${dir.path}\\${dir.name}`))
+      }
+
+      const files = all.filter(n => !n.isDirectory());
+      return files
+        .filter(file => file.name.indexOf('.mp4') !== -1 || file.name.indexOf('.mkv') !== -1)
+        // Without addning global path subs do not work for some reason
+        .map(file => `${file.path}/${file.name}`);
     }
   } catch (err) {
     console.error(err);
@@ -41,15 +48,16 @@ function createPlaylistXML(fileNames) {
 
   fileNames.forEach((file, i) => {
     root.ele('track')
+      // has to be regular slashes "/" not "\"
       .ele('location').txt(`file:///${file}`).up()
       .ele('duration').txt("0").up()
       .ele('extension', { "application": "http://www.videolan.org/vlc/playlist/0" })
-        .ele("vlc:id").txt(i).up()
-        .ele("vlc:option").txt(`audio-track=${audioTrack}`).up()
-        // Setting sub track to fake number to turn off subtitles by default
-        .ele("vlc:option").txt(`sub-track=${subsFlag ? 0 : fakeSubTrack}`).up()
+      .ele("vlc:id").txt(i).up()
+      .ele("vlc:option").txt(`audio-track=${audioTrack}`).up()
+      // Setting sub track to fake number to turn off subtitles by default
+      .ele("vlc:option").txt(`sub-track=${subsFlag ? 0 : fakeSubTrack}`).up()
       .up()
-    .up();
+      .up();
   })
 
   root.up()
@@ -60,23 +68,24 @@ function createPlaylistXML(fileNames) {
 }
 
 /**
- * @param {string[]} folderNames - names of folders in current catalog
+ * @param {string} path - path to folder with videos
  * @return {void}
  */
-function createPlaylistFiles(folderNames) {
-  folderNames.forEach(folderName => {
-    const names = getVideosNames(`${rootFolder}/${folderName}`);
-    const xmlString = createPlaylistXML(names);
+function createPlaylistFiles(path) {
+  const names = getVideosNames(path);
+  if (names.length !== 0) {
 
+    const xmlString = createPlaylistXML(names);
     try {
-      const fileName = `${rootFolder}/${folderName}/${folderName}.xspf`;
-      writeFileSync(fileName, xmlString);
-      console.log(`File "${folderName}.xspf" is created`);
+      const fileName = path.split("\\").pop();
+      const filePath = `${path}\\${fileName}.xspf`;
+      writeFileSync(filePath, xmlString);
+      console.log(`File "${filePath}" is created`);
     } catch (err) {
       console.error(err);
     }
-  });
+  }
 }
 
-createPlaylistFiles(folderNamesReaden);
+createPlaylistFiles(rootFolder);
 
