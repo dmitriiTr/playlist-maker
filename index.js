7e1738edc95abb2641af5c2c89fe29e324b830aa
dmitriiTr <56300102+dmitriiTr@users.noreply.github.com>
2026-03-02 15:55:06 +0300

    format improved

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // @ts-check
 
-import { existsSync, readdirSync, writeFileSync } from 'node:fs';
-import { getFileExtension, partition } from './utils.js';
+import { existsSync, readdirSync, writeFileSync } from "node:fs";
+import { getFileExtension, partition } from "./utils.js";
 
-import { VIDEO_FILES_EXTENSIONS } from './constants.js';
-import { create } from 'xmlbuilder2';
-import minimist from 'minimist';
+import { VIDEO_FILES_EXTENSIONS } from "./constants.js";
+import { create } from "xmlbuilder2";
+import minimist from "minimist";
 import pathModule from "path";
 
 /**
@@ -16,19 +16,24 @@ function getVideosNames(pathToVideos) {
   try {
     if (!existsSync(pathToVideos)) {
       console.error("not found");
-    }
-    else {
+    } else {
       const allElements = readdirSync(pathToVideos, { withFileTypes: true });
-      const [dirs, files] = partition(allElements, n => n.isDirectory());
+      const [dirs, files] = partition(allElements, (n) => n.isDirectory());
       if (args.r) {
         // recursively creating playlists for subfolders
-        dirs.forEach(dir => createPlaylistFiles(`${dir.parentPath}\\${dir.name}`));
+        dirs.forEach((dir) =>
+          createPlaylistFiles(`${dir.parentPath}\\${dir.name}`),
+        );
       }
 
-      return files
-        .filter(file => VIDEO_FILES_EXTENSIONS.has(getFileExtension(file.name)))
-        // Without adding global path subs do not work for some reason
-        .map(file => `${file.parentPath}/${file.name}`);
+      return (
+        files
+          .filter((file) =>
+            VIDEO_FILES_EXTENSIONS.has(getFileExtension(file.name)),
+          )
+          // Without adding global path subs do not work for some reason
+          .map((file) => `${file.parentPath}/${file.name}`)
+      );
     }
   } catch (err) {
     console.error(err);
@@ -42,42 +47,67 @@ function getVideosNames(pathToVideos) {
  */
 function createPlaylistXML(fileNames) {
   const fakeSubTrack = 99;
-  const { subTrack, noSub, subFile, audioTrack } = args;
+  const { subTrack, noSub, subFile, audioTrack = 0 } = args;
   const subValue = subTrack
     ? subTrack
-    // Setting sub track to fake number to turn off subtitles
-    : noSub ? fakeSubTrack : 0;
+    : noSub // Setting sub track to fake number to turn off subtitles
+      ? fakeSubTrack
+      : 0;
 
   /**
-  * @param {number} i - index of file
-  * @return {string} attribute value
-  */
-  const subFileForVideo = i => subFile.replace("$", (i + 1).toLocaleString('en-US', { minimumIntegerDigits: 2 }));
+   * @param {number} i - index of file
+   * @return {string} attribute value
+   */
+  const subFileForVideo = (i) =>
+    subFile.replace(
+      "$",
+      (i + 1).toLocaleString("en-US", { minimumIntegerDigits: 2 }),
+    );
 
-  const root = create({ version: '1.0', encoding: "UTF-8" })
-    .ele('playlist', { xmlns: 'http://xspf.org/ns/0/', "xmlns:vlc": "http://www.videolan.org/vlc/playlist/ns/0/" })
-    .ele('title').txt("Title").up().ele('trackList');
+  const root = create({ version: "1.0", encoding: "UTF-8" });
+  const playlist = root.ele("playlist", {
+    xmlns: "http://xspf.org/ns/0/",
+    "xmlns:vlc": "http://www.videolan.org/vlc/playlist/ns/0/",
+  });
+
+  playlist.ele("title").txt("Title").up();
+  const trackList = playlist.ele("trackList");
 
   fileNames.forEach((fileName, i) => {
-    const track = root.ele('track')
-      // has to be regular slashes "/" not "\"
-      .ele('location').txt(`file:///${encodeURIComponent(fileName)}`).up()
-      .ele('duration').txt("0").up()
-      .ele('extension', { "application": "http://www.videolan.org/vlc/playlist/0" })
-      .ele("vlc:id").txt(i.toString()).up()
-      .ele("vlc:option").txt(`audio-track=${audioTrack}`).up();
-
-    // Either add new sub file or selecting from existing
-    if (subFile) {
-      track.ele("vlc:option").txt(`sub-file=${subFileForVideo(i)}`).up();
-    } else {
-      track.ele("vlc:option").txt(`sub-track=${subValue}`).up();
-    }
-    root.up()
+    const track = trackList.ele("track");
+    // has to be regular slashes "/" not "\"
+    track
+      .ele("location")
+      .txt(`file:///${encodeURIComponent(fileName)}`)
       .up();
+
+    track.ele("duration").txt("0").up();
+
+    const extension = track.ele("extension", {
+      application: "http://www.videolan.org/vlc/playlist/0",
+    });
+
+    extension.ele("vlc:id").txt(i.toString()).up();
+
+    extension.ele("vlc:option").txt(`audio-track=${audioTrack}`).up();
+
+    const subtitleOption = extension.ele("vlc:option");
+
+    // Either selecting sub file from existing or adding suggested
+    if (subFile) {
+      subtitleOption.txt(`sub-file=${subFileForVideo(i)}`);
+    } else {
+      subtitleOption.txt(`sub-track=${subValue}`);
+    }
+
+    subtitleOption.up();
+    extension.up();
+    track.up();
+    playlist.up();
   });
-  root.up()
-    .up();
+
+  trackList.up();
+  playlist.up();
 
   // convert the XML tree to string
   return root.end({ prettyPrint: true });
@@ -88,7 +118,6 @@ function createPlaylistFiles(dirPath) {
   const names = getVideosNames(dirPath);
 
   if (names.length !== 0) {
-
     const xmlString = createPlaylistXML(names);
     try {
       const folderName = pathModule.basename(pathModule.resolve(dirPath));
